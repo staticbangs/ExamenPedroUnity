bf8c161fd447ca48a86c4ccc972829126d001311
Luciana <LV>
2015-12-02 19:00:13 +0100

    Tarea 2 - Disparo

---
+++ ExamenLuciana/Assets/Scripts/Bala.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bala : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D objeto){
+		if(objeto.transform.tag =="enemigo"){
+			Destroy(objeto.gameObject);
+			Destroy(gameObject);
+		}
+		
+	}
+}
---
+++ ExamenLuciana/Assets/Scripts/Disparo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparo : MonoBehaviour {
+
+	public GameObject bala; //variable del objeto que usaremos como proyectil
+	public Vector2 fuerzadisparo; //fuerza con la que lanzamos la vala
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		disparo(); //llamamos a la funcion en todo momento
+	}
+
+	void disparo(){
+	
+		if (Input.GetKeyDown(KeyCode.Space)){ //cuando presionamos espacio
+		
+			GameObject nueva_bala = (GameObject) Instantiate(bala,transform.position, transform.rotation); //creamos un nuevo proyectil
+			nueva_bala.GetComponent<Rigidbody2D>().AddForce(fuerzadisparo); //a este proyectil le añadimos  una fuerza
+			Destroy(nueva_bala,2); //destruimos el proyectil tras 2 segundos
+
+		}
+	}
+
+
+}
