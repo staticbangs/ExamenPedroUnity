d16b6ca08bc8d18d10fe940f3a79e0ad831ccac6
Luciana <LV>
2015-12-02 19:09:35 +0100

    Tarea 3 - Enemigos

---
+++ ExamenLuciana/Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoEnemigo : MonoBehaviour {
+
+	Rigidbody2D rg; //rigidbody del objeto
+	public int Velocidad; //Velocidad con la que se moveran los enemigos
+
+	// Use this for initialization
+	void Start () {
+
+		rg = GetComponent <Rigidbody2D>(); //llamamos al rigidbody para poder ponerle fuerza luego
+		Destroy (gameObject,2.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rg.velocity = new Vector2(0, Velocidad); //La velocidad del rigidbody, sera la que añadamos en la variable velocidad
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D objeto){
+	if(objeto.transform.tag == "Player"){
+			Destroy(objeto.gameObject);
+		
+		}
+	}
+}
